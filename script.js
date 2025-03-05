a95cec428d0cb26de19dc387cb68cc45035a0070
BaxolotlChan <bkchua22@src.pshs.edu.ph>
2025-03-05 14:08:07 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function verifyMessage(){
+  let message = prompt("Enter the message to be verified: ");
+
+  if (message.toUpperCase().indexOf("FR") >= 0 ){
+    document.getElementById("result").innerHTML = `Your message "${message}" is legitimate!`;
+  } else if (message.toUpperCase().indexOf("AI") >= 0 ){
+    document.getElementById("result").innerHTML = `Your message "${message}" is tampered with!`;
+  } else if (message.toUpperCase().indexOf("FR") >= 0 && message.toUpperCase().indexOf("AI") >= 0){
+    document.getElementById("result").innerHTML = `Your message "${message}" is legitimate!`;
+  } else if (message.indexOf("Ai") >= 0 || message.indexOf("aI") >= 0 ){
+    document.getElementById("result").innerHTML = `Your message "${message}" is tampered with!`;
+  } else {
+    document.getElementById("result").innerHTML = `Your message "${message}" is yet to be encoded.`;
+  }
+}
